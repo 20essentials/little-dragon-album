cbab540240a41d7410b2f3d171808c44c637fc68
DominicDev <s0l0estudi4r@gmail.com>
2025-12-02 12:41:30 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'LITTLE DRAGON';
+export let nameSongs = [
+	"RITUAL UNION", /*1*/
+	"CRYSTALFILM", /*2*/
+	"HIGH", /*3*/
+	"CAT RIDER", /*4*/
+	"PINK CLOUD", /*5*/
+	"NO LOVE", /*6*/
+	"UNDERBART", /*7*/
+	"ONLY ONE", /*8*/
+	"MIRROR", /*9*/
+	"LET GO", /*10*/
+	"PRETTY GIRLS", /*11*/
+	"KLAPP KLAPP", /*12*/
+	"KILLING ME", /*13*/
+	"NEVER NEVER", /*14*/
+	"BRUSH THE HEAT", /*15*/
+	"AFTER THE RAIN", /*16*/
+	"LOOKING GLASS", /*17*/
+	"SWIMMING", /*18*/
+	"STROBE LIGHT", /*19*/
+	"CONSTANT SURPRISES", /*20*/
+]
